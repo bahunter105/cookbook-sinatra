5b2246e8558c12411c21e948077c1c6d697ae85a
Hunter Bourgeois <bahunter@gmail.com>
2022-01-09 14:53:04 +0100

    added a form and post to add new recipe

--- app.rb
+++ app.rb
@@ -2,13 +2,14 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require_relative 'cookbook'
+require_relative 'recipe'
 set :bind, '0.0.0.0'
 # configure :development do
 #   use BetterErrors::Middleware
 #   BetterErrors.application_root = File.expand_path('..', __FILE__)
 # end
 
-csv_file = File.join(__dir__, 'recipes.csv')
+csv_file = File.join(__dir__, 'lib/recipes.csv')
 cookbook = Cookbook.new(csv_file)
 
 get '/' do
@@ -21,4 +22,15 @@ get '/cookbook/:index_no' do
   recipe_index = params['index_no'].to_i
   @recipe = @cookbook.all[recipe_index]
   erb :show
+end
+
+post '/' do
+  @cookbook = cookbook
+  # pry
+  name = params[:name]
+  description = params[:description]
+  new_recipe = Recipe.new(name, description)
+  @cookbook.add_recipe(new_recipe)
+  @cookbook.save_csv
+  erb :index
 end
